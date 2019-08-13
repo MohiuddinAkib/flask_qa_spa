dd30409a3d8f090916203f79505496938f887a8d
md akib <mohiuddinmostafakamal@gmail.com>
2019-08-13 10:18:15 +0600

    created vue client

--- flask_rest_api/__init__.py
+++ flask_rest_api/__init__.py
@@ -3,11 +3,11 @@ from .user.models import User
 from .routes.main import main
 from .routes.auth import auth
 from .question.models import Question
-from flask_rest_api.user.views import UserVIew
 from flask_rest_api.user.routes import user
+from flask_rest_api.user.views import UserVIew
 from flask_admin.contrib.sqla import ModelView
-from .extensions import db, ma, migrate, jwt, admin
 from flask_rest_api.question.routes import question
+from .extensions import db, ma, migrate, jwt, admin, cors
 
 
 def create_app(config_file='settings.py'):
@@ -25,6 +25,8 @@ def create_app(config_file='settings.py'):
     jwt.init_app(app)
     # Init admin
     admin.init_app(app)
+    # Init cors
+    cors.init_app(app)
     # register blueprints
     app.register_blueprint(main, url_prefix='/api')
     app.register_blueprint(auth, url_prefix='/api/auth')
